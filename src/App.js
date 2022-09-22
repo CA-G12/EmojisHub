8f4a2bb5c67196f0ca786d3636df7bf898a69af0
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-22 10:06:11 +0300

    Added needed components and states for the App componenet the parent of all

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React, { Component, useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  state = {  } 
-  render() { 
+  state = { selectedCategory: "all", selectedGroup: null };
+  updateState = (object) => {
+    this.setState(object);
+  };
+  render() {
+    console.log(this.state.selectedCategory, this.state.selectedGroup);
     return (
-    <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>);
+      <div className="main">
+        <Sidebar updateState={this.updateState} />
+      </div>
+    );
   }
 }
- 
-export default App;
 
+export default App;
